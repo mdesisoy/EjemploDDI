2beb7c6a14b7cdf36ce257782f41c7c2bb534145
mdesisoy <lozano.lemar21@zaragoza.salesianos.edu>
2022-11-10 09:03:12 +0100

    Cambios en las sql

--- EjemploDDI/src/main/resources/scripts/create.sql
+++ EjemploDDI/src/main/resources/scripts/create.sql
@@ -1,4 +1,4 @@
 create table IF NOT EXISTS TITULACION(
-id bigint auto_increment,
+id bigint auto_increment primary key,
 titulo varchar(25)
 );
--- EjemploDDI/target/classes/scripts/create.sql
+++ EjemploDDI/target/classes/scripts/create.sql
@@ -1,4 +1,4 @@
-create table IF NOT EXISTS USER(
-	nombre varchar(25),
-	apellido varchar(25)
+create table IF NOT EXISTS TITULACION(
+id bigint auto_increment primary key,
+titulo varchar(25)
 );
